6b6d3d66a1590aded32df1e7066e6709fca29b5a
marvinjaylagang01 <marvinjaylagang01@gmail.com>
2023-12-24 06:45:00 +0800

    Wiki page initial commit

---
+++ src/Components/WikiSearchArea.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { Form, Button} from "react-bootstrap"
+
+export default function WikiSearchArea({setForms,setLoading,refreshEffect,setHeader}){
+    const [title, setTitle] = useState('');
+
+    const submitSearchRequest = () =>{
+        setLoading(true); 
+        fetch(`${process.env.REACT_APP_API_URL}/api/articles/article`,{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            },
+            body: JSON.stringify({
+                title: title.replace(/\\/g, "\\\\")
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            setForms(data)
+            setLoading(false);
+            setHeader('Custom Search')
+        })
+
+    }
+    const reset = () =>{
+        setTitle(''); 
+        refreshEffect();
+        setHeader('All Forms & Docs')
+    }
+    return(
+        <div className="d-flex">
+            <Form.Control
+            style={{borderRadius:'8px'}}
+            type="text"
+            placeholder="Search..."
+            onChange={(e) => setTitle(e.target.value)}
+            className='custom-search-input'
+            value={title} 
+            />
+            <div className="d-flex align-items-center">
+                <Button className="d-flex ml-1 button-bg" style={{boxShadow:'none', height:'30px'}} size='sm' onClick={submitSearchRequest}>Search</Button>
+                <Button className="d-flex ml-1 button-cancel"  style={{boxShadow:'none', height:'30px'}} size='sm' onClick={reset}>Reset</Button>
+            </div>
+            
+        </div>
+    )
+}
--- src/Pages/Wiki.js
+++ src/Pages/Wiki.js
@@ -1,8 +1,41 @@
-
+import { useContext, useState, useEffect } from "react"
+import UserContext from '../userContext';
+import FormCard from "../Components/FormCard";
+import AddNewForm from "../Components/AddNewForm";
+import LoaderTwo from "../Components/Subcomponents/loader/LoaderTwo";
+import WikiSearchArea from "../Components/WikiSearchArea";
+import NoFormsFound from "../Components/NoFormsFound";
 
 export default function Wiki(){
-    return(
+    const {user} = useContext(UserContext);
+    const [forms, setForms]=useState(null);
+    const [loading, setLoading] = useState(true); // Add loading state
+    const [header, setHeader] = useState('All Forms & Docs')
+
+    const refreshEffect = () =>{
+        setLoading(true); 
+        fetch(`${process.env.REACT_APP_API_URL}/api/articles/`,{
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            setForms(data)
+            setLoading(false);
+        })
+    }
+    return( 
         <>
+        <div className="row">
+            <div className="mb-0 col-5">
+                <h4 className="text-muted">Wiki</h4>
+            </div>
+            <div className="col-6 ml-auto">
+                <WikiSearchArea setForms={setForms} setLoading={setLoading} refreshEffect={refreshEffect} setHeader={setHeader}/>
+            </div>
+        </div>
+        <hr />
         </>
     )
 }
