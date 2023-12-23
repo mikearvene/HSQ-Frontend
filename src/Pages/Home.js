bab048baad4f850baf65b5e0bcde77dafa27f65d
marvinjaylagang01 <marvinjaylagang01@gmail.com>
2023-12-24 06:42:38 +0800

    Added Some sort of header

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,6 +5,12 @@ export default function Home(){
     
     return(
         <>
+        <div className="row">
+            <div className="mb-0 col-5">
+                <h4 className="text-muted">Home</h4>
+            </div>
+        </div>
+        <hr />
         <QuickLinks />
         </>
     )
