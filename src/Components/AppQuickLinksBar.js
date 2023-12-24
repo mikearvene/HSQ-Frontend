645c4db4f71a3dca0ee15e7097fc2d218b5d0534
marvinjaylagang01 <marvinjaylagang01@gmail.com>
2023-12-24 10:05:57 +0800

    New Quick links implementation

---
+++ src/Components/AppQuickLinksBar.js
@@ -0,0 +1,29 @@
+import { Container, Row } from "react-bootstrap";
+import LinkCard from "./LinkCard";
+import {links} from "../Util/externalLinks"
+
+export default function AppQuickLinksBar () {
+
+    return(
+        <>
+        <div className="sidenavbar" >
+            {/* Below serves as a header */}
+            <div className="p-2">
+                <p className="text-muted text-center smallest">Company External Tools</p>
+                <hr />
+            </div>
+            <Container>
+                <Row className="mb-4">
+                    {/* Render LinkCard's Here */}
+                    {/* Map each link to a LinkCard */}
+                    {links.map((link, index) => (
+                      <LinkCard key={index} siteName={link.siteName} link={link.link} icon={link.icon}/>
+                    ))}
+                </Row>
+            </Container>
+            
+
+        </div>
+        </>
+    )
+}
---
+++ src/Util/externalLinks.js
@@ -0,0 +1,79 @@
+const links = [
+    {
+      siteName: "Microsoft Teams",
+      link: "https://teams.microsoft.com/v2/",
+      icon: '/link-icons/teams.svg'
+    },
+    {
+        siteName: "Outlook",
+        link: "https://outlook.office.com/mail/",
+        icon: '/link-icons/outlook.svg'
+    },
+    {
+      siteName: "Online Jobs PH",
+      link: "https://www.onlinejobs.ph/",
+      icon: '/link-icons/onlinejobs.ph.png'
+    },
+    {
+      siteName: "Canva",
+      link: "https://www.canva.com/",
+      icon: '/link-icons/canva-icon.svg'
+    },
+    {
+      siteName: "Fiverr",
+      link: "https://www.fiverr.com/",
+      icon: '/link-icons/fiverr-icon.svg'
+    },
+    {
+      siteName: "Test Gorilla",
+      link: "https://www.testgorilla.com/",
+      icon: '/link-icons/TestGorilla.svg'
+    },
+    {
+      siteName: "Deel",
+      link: "https://www.deel.com/",
+      icon: '/link-icons/Deel.svg'
+    },
+    {
+      siteName: "Xero",
+      link: "https://www.xero.com/ph/",
+      icon: '/link-icons/Xero.png'
+    },
+    {
+      siteName: "Trello",
+      link: "https://trello.com/",
+      icon: '/link-icons/trello-tile.svg'
+    },
+    {
+      siteName: "Youtube Music",
+      link: "https://music.youtube.com/",
+      icon: '/link-icons/link-icon.svg'
+    },
+    {
+      siteName: "Spotify",
+      link: "https://artists.spotify.com/home",
+      icon: '/link-icons/link-icon.svg'
+    },
+    {
+      siteName: "Apple Music",
+      link: "https://artists.apple.com/",
+      icon: '/link-icons/link-icon.svg'
+    },
+    {
+      siteName: "Ditto Music",
+      link: "https://dittomusic.com/en",
+      icon: '/link-icons/link-icon.svg'
+    },
+    {
+      siteName: "AirGigs",
+      link: "https://www.airgigs.com/",
+      icon: '/link-icons/link-icon.svg'
+    },
+    {
+      siteName: "Logic Pro X",
+      link: "https://www.apple.com/logic-pro/",
+      icon: '/link-icons/link-icon.svg'
+    }
+  ];
+
+  export { links };
