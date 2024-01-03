c5380bbf5303f9e56fd91730b96899a7176032ac
marvinjaylagang01 <marvinjaylagang01@gmail.com>
2024-01-04 00:16:22 +0800

    Updated the fontColor

--- src/Components/NoFormsFound.js
+++ src/Components/NoFormsFound.js
@@ -3,7 +3,7 @@ export default function NoFormsFound(){
 
     return(
         <>
-            <div className="d-flex justify-content-center align-items-center" style={{height:'50vh'}}>
+            <div className="d-flex justify-content-center align-items-center" style={{height:'50vh', color:'#516473'}}>
                 <h4>No Forms Found</h4>
             </div>
             
