f548158fe2265e5243e9ca8480ca5e81d9795d92
Marvin Jay Lagang <[marvinjaylagang01@gmail.com]>
2024-01-06 16:46:05 +0800

    minor layout touches

--- src/Components/Subcomponents/UserInfoBox.js
+++ src/Components/Subcomponents/UserInfoBox.js
@@ -7,12 +7,12 @@ export default function UserInfoBox  (user)  {
         <Container fluid>
             <div className="row align-items-center mt-2" style={{height:`68px`, borderRadius:'8px',borderColor:'#575757', border:"solid", borderWidth:"1px"}}>
                     {user.user.profilePicture !== "false" ? 
-                    <div className="col-3">
+                    <div className="col-3 d-flex justify-content-start align-items-center">
                     <img 
                     src={user.user.profilePicture}
                     className="" 
                     alt="profile-picture" 
-                    style={{ borderRadius: '50%', width: '48px', height: '48px', border: '1px solid #F3F3F3', objectFit: "cover" }}
+                    style={{ borderRadius: '50%', width: '48px', height: '48px', borderColor:'#575757', border:"solid", borderWidth:"1px", objectFit: "cover" }}
                     />
                     </div>
                     :
