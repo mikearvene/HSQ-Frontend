4e259b47dda6019d9327d646ac0d0cc34dff9087
Marvin Jay Lagang <[marvinjaylagang01@gmail.com]>
2024-01-06 16:32:43 +0800

    minor update

--- src/Components/Subcomponents/NavUserIcon.js
+++ src/Components/Subcomponents/NavUserIcon.js
@@ -16,12 +16,15 @@ export default function NavUserIcon({setIsOpen, isOpen, user}){
         <a style={{cursor:'pointer'}} onClick={handleClick}>
             {user.profilePicture !== "false" ? 
             <div className="d-flex">
-            <img 
-            src={user.profilePicture}
-            className="" 
-            alt="profile-picture" 
-            style={{ borderRadius: '50%', width: '28px', height: '28px', border: '1px solid #F3F3F3', objectFit: "cover" }}
-            />
+            <div>
+                <img 
+                src={user.profilePicture}
+                className="" 
+                alt="profile-picture" 
+                style={{ borderRadius: '50%', width: '28px', height: '28px', border: '1px solid #F3F3F3', objectFit: "cover" }}
+                />
+            </div>
+            
             <img 
             className="mt-2 ml-1"
             src='/icons/dropdown-icon.svg' 
--- src/Components/Subcomponents/UserInfoBox.js
+++ src/Components/Subcomponents/UserInfoBox.js
@@ -6,9 +6,20 @@ export default function UserInfoBox  (user)  {
     return (
         <Container fluid>
             <div className="row align-items-center mt-2" style={{height:`68px`, borderRadius:'8px',borderColor:'#575757', border:"solid", borderWidth:"1px"}}>
+                    {user.user.profilePicture !== "false" ? 
+                    <div className="col-3">
+                    <img 
+                    src={user.user.profilePicture}
+                    className="" 
+                    alt="profile-picture" 
+                    style={{ borderRadius: '50%', width: '48px', height: '48px', border: '1px solid #F3F3F3', objectFit: "cover" }}
+                    />
+                    </div>
+                    :
                     <div className="col-3">
                         <img src="/icons/sidebar-user-icon.svg" alt="sidebar-user-icon.svg" />
                     </div>
+                    }
                     <div className="col-8 pr-0 justify-content-start align-items-center">
                     <span className="text-muted small">{firstName} {lastName}</span>
                     <br />
