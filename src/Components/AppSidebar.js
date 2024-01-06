e76c3708f86410f326f1b2532db9853fdc018904
Marvin Jay Lagang <[marvinjaylagang01@gmail.com]>
2024-01-06 17:57:40 +0800

    added new badge component

--- src/Components/AppSidebar.js
+++ src/Components/AppSidebar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import UserContext from '../userContext';
 import UserInfoBox from './Subcomponents/UserInfoBox';
 import ComingSoonBadge from './Subcomponents/ComingSoonBadge';
+import NewFeatureBadge from './Subcomponents/NewFeatureBadge';
 
 const AppSidebar = () => {
   const {user} = useContext(UserContext);
@@ -29,7 +30,7 @@ const AppSidebar = () => {
         <Link to='/' className=''>Home</Link>
         <Link to='/forms-repository' className=''>Forms Repository</Link>
         <Link to={'/wiki'}>Wiki</Link>
-        <Link to={'/my-profile/detail'}>My Profile</Link>
+        <Link to={'/my-profile/detail'}>My Profile <span style={{position:'absolute', bottom:'40%', right:'48%'}}><NewFeatureBadge/></span></Link>
         <p className='user-select-none'><span className='text-muted'>My Calendar <ComingSoonBadge/></span></p>
         <p className='user-select-none'><span className='text-muted'>News & Updates <ComingSoonBadge/></span></p>
         <p className='user-select-none'><span className='text-muted'>Team Directory <ComingSoonBadge/></span></p>
---
+++ src/Components/Subcomponents/NewFeatureBadge.js
@@ -0,0 +1,7 @@
+export default function NewFeatureBadge (){
+    return(
+        <div className="d-inline-flex align-items-start p-1" style={{backgroundColor:'#F22F41', borderRadius:'3px'}}>
+                <span className="smallest text-white">New!</span>
+        </div>
+    )
+}
