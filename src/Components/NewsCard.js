a5ec3f6c206826a4ede8e6816cf8085f226b2109
Marvin Jay Lagang <[marvinjaylagang01@gmail.com]>
2024-01-10 22:35:11 +0800

    second image preview issue fix attempt

--- src/Components/NewsCard.js
+++ src/Components/NewsCard.js
@@ -38,7 +38,7 @@ export default function NewsCard ({data}){
         }
     
         setTimeDiff(displayTimeDiff);
-      }, [imageCount]);
+      }, [data]);
 
     return(
         <Col md={12} className="mb-3 p-3" style={{backgroundColor: 'rgba(81, 100, 115, 0.3)', borderRadius:'5px'}}>
