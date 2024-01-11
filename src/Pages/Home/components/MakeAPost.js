72920f4df92e064c050a6f7a39fc46686747d3fc
Marvin Jay Lagang <marvin.lagang@hollywoodsq.com>
2024-01-11 16:33:21 +0800

    added ProfileSKeleton

--- src/Pages/Home/components/MakeAPost.js
+++ src/Pages/Home/components/MakeAPost.js
@@ -1,26 +1,41 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import ProfileSkeleton from "../../../Components/Subcomponents/ProfileSkeleton";
 export default function MakeAPost({user}){
-
+    const [imageLoading, setImageLoading] = useState(true);
     const [message, setMessage]=useState("")
-    console.log(user)
+    const [showModal, setShowModal] = useState(false);
     const openModal = () =>{
 
     }
+
+    const handleImageLoad = () => {
+        setImageLoading(false);
+    };
+    useEffect(()=>{
+        user.profilePicture === 'false'? setImageLoading(false) : setImageLoading(true)
+    },[user.profilePicture])
+    console.log(user)
     return(
         <>
         <div className="border p-3 row" style={{backgroundColor: 'rgba(81, 100, 115, 0.3)', borderRadius:"8px", width:'100%'}}>
+        
             <div className="d-flex justify-content-center align-items-center col-2">
+                
                 {user.profilePicture !== 'false' ? 
                 <img 
                 src={user.profilePicture} 
                 alt="user.profilePicture" 
+                onLoad={handleImageLoad} 
                 style={{ borderRadius: '50%', width: '45px', height: '45px', objectFit: "cover" }}/>
                 :
                 <img
                 src='/icons/sidebar-user-icon.svg'
-                alt="defaultPic" 
+                alt="defaultPic"
                 style={{ borderRadius: '50%', width: '45px', height: '45px', objectFit: "cover" }}/>
                 }
+                {imageLoading? 
+                <ProfileSkeleton borderRadius={'50%'} height={'45px'} width={'45px'}/>
+                :<></>}
             </div>
             <div className="d-flex justify-content-center align-items-center col-7" style={{width:'100%'}}  > 
                 <button className="" onClick={openModal}  style={{ borderRadius: '25px',width:'100%', backgroundColor:'white', borderColor:'muted', borderWidth:'0.5px' }}>
