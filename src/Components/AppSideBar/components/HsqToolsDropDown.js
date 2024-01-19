c2184f68cf5e71a91eb8e40af1a8e3c9a5b3c574
Marvin Jay Lagang <marvin.lagang@hollywoodsq.com>
2024-01-20 01:04:10 +0800

    minor edit

--- src/Components/AppSideBar/components/HsqToolsDropDown.js
+++ src/Components/AppSideBar/components/HsqToolsDropDown.js
@@ -2,7 +2,7 @@
 export default function HsqToolsDropDown({ComingSoonBadge, isHsqToolsDropped}){
     return(
         <>
-            <div className={`ml-4 mt-2 ${isHsqToolsDropped ? 'd-none' : null}`}>
+            <div className={`ml-4 mt-2 ${!isHsqToolsDropped ? 'd-none' : null}`}>
                 <div className="mb-2">
                     <span className="user-select-none text-muted">Form Builder <ComingSoonBadge/> </span>
                 </div>
