e21d80a4eb2b3b008ef415904295c307b6621fe5
Marvin Jay Lagang <marvin.lagang@hollywoodsq.com>
2024-01-20 14:58:21 +0800

    added comment

--- src/Pages/Home/components/HomeCard.js
+++ src/Pages/Home/components/HomeCard.js
@@ -4,7 +4,10 @@ export default function HomeCard({data, user, fetchData}){
         <>
         <Col md={12} className="mr-5 mb-3">
         <div className="border p-3 row" style={{backgroundColor: 'rgba(81, 100, 115, 0.3)', borderRadius:"8px", width:'100%', height:'200px'}}> 
-        
+        <div className="general-center">
+            <h6 className="text-muted">Non-Urgent updates will be posted here (for example: Holiday Announcements, Company Engagement Activity Announcements, etc.)</h6>
+        </div>
+            
         </div>
         </Col>
         </>
