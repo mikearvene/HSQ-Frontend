0136171e37c7f67d09508234e06b9fcdd0719a5e
Marvin Jay Lagang <marvin.lagang@hollywoodsq.com>
2024-01-20 14:19:29 +0800

    home card initial commit

---
+++ src/Pages/Home/components/HomeCard.js
@@ -0,0 +1,12 @@
+import { Col } from "react-bootstrap"
+export default function HomeCard({data, user, fetchData}){
+    return(
+        <>
+        <Col md={12} className="mr-5 mb-3">
+        <div className="border p-3 row" style={{backgroundColor: 'rgba(81, 100, 115, 0.3)', borderRadius:"8px", width:'100%', height:'200px'}}> 
+        
+        </div>
+        </Col>
+        </>
+    )
+}
